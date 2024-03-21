7b7712bf7d7d2a33d4c0908d783cd1cd6e12b274
abeer025 <abeeriqbalahmed@gmail.com>
2024-03-21 23:46:53 +0500

    commit

---
+++ app.js
@@ -0,0 +1,474 @@
+// ====== Chap 1 =======//
+// Task 1 
+// var firstName = "Abeer";
+// var lastName = "iqbal";
+// var email = "abeeriqbalahmed@gmail.com";
+// var phoneNumber = "03132938814";
+// var password = "A7ggg@8";
+// alert(firstName);
+// alert(lastName);
+// alert(email);
+// alert(phoneNumber);
+// alert(password);
+
+// task 2
+// alert("your'e learning Javascript!");
+
+// task 3
+// alert("Welcome to Javascript");
+
+// ====== Chap 2 =======//
+// Task 1
+// var camelCase = "camel case have start first letter with small then other letetr is ";
+
+// Task 2
+// var Afor ;
+// Afor = "apple";
+// console.log(a);
+
+// Task 3
+// var teamName = "The Abeer's Team";
+// alert(teamName);
+
+// Task 4
+// var bestName = "Charlie";
+// var bestName = "Charlie Man";
+// console.log(bestName);
+
+// ====== Chap 3 =======//
+
+// Task 1
+// var caseQty ;
+// Task 2
+// caseQty = 144;
+// console.log(caseQty);
+
+// Task 3
+// var num = "9";
+// console.log(num);
+
+// Task 4
+// var num ;
+// num = 2 + 2
+// console.log(num);
+
+// Task 5
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal);
+
+// Task 6
+// var num = 20;
+// num = 205
+// console.log(num);
+
+// ====== Chap 4 =======//
+
+// Task 1
+// var product cost = 3.45 ;
+//'didn't allow to space in variable name its illegal statement'// ;
+// correct statement is ://
+// productCost = 3.45;
+// console.log(productCost);
+
+// Task 2
+// var Nameofband; //
+// var nameOfBand; // camelCase
+
+// Task 3
+// var costPrice = 50 ; //legal name variable//
+// console.log(costPrice);
+
+// Task 4
+// var firstName = "Abeer";
+// var lastName = "Iqbal";
+// fullname = firstName + " " + lastName;
+// console.log(fullname);
+
+//Task 5
+// *RULES for variable names*//
+// var asd = '' //legal
+// var 123 = '' //illegal
+// var ab12 = "" //legal
+// var 12ab = "" //illegal
+// var var = "" //ilegal
+// var #&*) = "" //illegal
+// var ab&s = "" //illegal
+
+// ====== Chap 5 =======//
+
+//Task 1
+//the name of the remainder is ;
+//modulus operator '%' //
+
+//Task 2
+// var num = 20 % 6; 
+// //the answer is "2" because is not divide evenly.//
+// console.log(num)
+
+//Task 3
+// var largeNum = 1000 * 2000;
+// console.log(largeNum)
+
+//Task 4
+// var num = 10;
+// console.log(num)
+// var substract = num - 5;
+// console.log(substract)
+
+//Task 5
+// var evenlyDivide = 10 % 10
+// console.log(evenlyDivide)
+
+//Task 6
+// alert("2*2 =" + 2*2)
+
+// ====== Chap 6 =======//
+
+// Task 1
+// var x = 9;
+// var x = x+1;
+// console.log(x);
+// y = ++x; //pre-increment
+// console.log(y);
+// z = x++; //post-increment
+// console.log(z);
+
+//Task 2
+// var x = 100;
+// y = --x; //pre-decrement is fastest way to reduce it.//
+// console.log(y);
+
+//Task 3
+// var x = 50;
+// var y = x++; // the answer is same value you assign before because its post-increment//
+// console.log(y)
+
+//Task 4
+// var y = 50;
+// var z = --y; // the answer 49 because its pre-decrement//
+// console.log(z)
+
+//Task 5
+// var num = 50; newNum = num--; 
+// console.log(newNum);
+
+//Task 6 
+// var num = 99; newNum = ++num;
+// console.log(newNum);
+
+// Task 7
+// var number = 54;
+// var incrementNumber = ++number
+// alert(`new value is: ${number}`);
+
+// ====== Chap 7 =======//
+
+//Task 1
+// var calculatedNum = 2 + (2 * 6); // the answer is 2 * 6 = 12 + 2 = 14  //
+// console.log(calculatedNum)
+
+//Task 2
+// var calculatedNum = (2 + 2) * 6; // the answer is 2 + 2 = 4 * 6 = 24  //
+// console.log(calculatedNum)
+
+//Task 3
+// var calculatedNum = (2 + 2) * (4 + 2); // the answer is 2 + 2 = 4 = 4 + 2 = 6 = 4 * 6 = 24  //
+// console.log(calculatedNum)
+
+// Task 4
+// var calculatedNum = ((2 + 2) * 4) + 2; // the answer is 2 + 2 = 4 * 4 = 18  //
+// console.log(calculatedNum)
+
+//Task 5
+// var calculatedNum = (2 + 2) * (4 + 10); // the answer is 2 + 2 = 4 = 4 + 10 = 14 * 4 = 56  //
+// console.log(calculatedNum)
+
+//Task 6
+// var calculatedNum = (2 + (2 * 4) + 10); // the answer is 2 * 4 = 8 + 2 + 10 + 10 = 20  //
+// console.log(calculatedNum)
+
+//Task 7
+// var calculatedNum = 4 / 2 * 4; // the answer is 4 / 2 = 2 * 4 = 8   //
+// console.log(calculatedNum)
+
+//Task 7
+// var calculatedNum = 4 / (2 * 4); // the answer is 4 / 2 = 2 * 4 = 8   //
+// console.log(calculatedNum)
+
+// ====== Chap 8 =======//
+
+//Task 1
+// var num = "2" + "2"; // the value of num is 22 because its a string, + two string concatenating in javascript  //
+// console.log(num)
+
+// Task 2
+// var message = ("HELLO," + "DOLLY ");  // the value of message is Hello,Dolly because its a string, + two string concatenating in javascript  //
+// console.log(message)
+// alert(message)
+// alert(`the value of message is: " ${message} " because its a string, + two string concatenating in javascript`)
+
+//Task 3
+// alert("33" + 3); // the message is "33" + 3 = 333 because its a string, + two string concatenating in javascript  //
+
+// Task 4
+// alert("Pakistan " + "Zindabad"); // the message is "Pakistan " + "Zindabad" because its a string, + two string concatenating in javascript  //
+
+//Task 5
+// var numWord = "K" + "2"; // the value of numWord is "K" + "2" = K2 because its a string, + two string concatenating in javascript  //
+// console.log(numWord)
+
+//Task 6
+// var compact =  'A'; 
+// var size =  '4';
+// var compactSize =  compact + size ; // the answer is "A" + "4" = A4 because its a string, + two string concatenating in javascript  //
+// console.log(compactSize)
+
+// ====== Chap 9 =======//
+
+//Task 1
+// var firstName = prompt("Enter your first name");
+// document.write(`Your first name is: ${firstName}`)
+
+// Task 2
+// var country = prompt("Country?", "China");
+// document.write(`Your country is: ${country}`)
+
+//Task 3
+// var yourName = prompt("Enter Your Name"); // "" inverted coma is necessary when you write a message in prompt//
+// document.write(`Your full name is: ${yourName}`)
+
+// Task 4
+// var favoriteSports = prompt("Which sports do you like?", "Cricket");
+// document.write(`My favorite sports is: ${favoriteSports}`)
+
+//Task 5
+// var hobbyQs = "What's your hobby?";
+// var hobby = "play cricket";
+//Task 6
+// var userInput = prompt(hobbyQs, hobby);
+// document.write(`<p>${hobbyQs}</p>`);
+// document.write(`<p>My hobby is: ${hobby}</p>`)
+
+// ====== Chap 10 =======//
+
+//Task 1
+// var city = "Karachi" 
+// if (city = "Karachi") {
+    //  console.log("The City OF Lights") }
+//Correct the above statement:
+
+// var city = prompt("Enter your city?","Karachi");
+// if (city === "Karachi"){
+// console.log("The City OF Lights")
+// document.write(`The City OF Lights`)
+// }
+
+// Task 2
+// var x = 2; y = 2;
+// //if condition true means x equal to y then show a display z //
+// if (x === y){
+//     z = prompt(" Enter the value of z:");
+//     var userInput = z
+// console.log("The Value of z is :", userInput)
+// document.write(`The Value of z is : ${userInput} `)
+// }
+
+//Task 3
+// var zipCode = prompt("Enter Your City ZipCode", 10010);
+// if (zipCode == 10010){
+//     alert("Karachi");
+// console.log("Karachi");
+// document.write('<h1> Karachi </h1>');
+// } else {
+//     alert("Please write correct city!");
+// console.log("Please write correct city!");
+// document.write("Please write correct city!");
+// }
+
+
+// Task 4
+// var x = 1; y = 2;
+// if (x === y){
+//     z = prompt(" ");
+//     var userInput = 
+// console.log(":", userInput)
+// document.write(`: ${userInput} `)
+// }
+
+// ====== Chap 11 =======//
+
+//Task 1
+// var city = Prompt( "Enter your city:" ,"Karachi" )
+// //!== means city not equal to Karachi //
+// if (city !== "Karachi") {
+//      console.log("Please write correct city") 
+//     }
+
+// Task 2
+// var avgRuns = prompt("Enter a average of a palyer in a international cricket (should be under 50)",50)  
+// //>= means avgRuns greaterthan or equal to 50 //
+// if (avgRuns >= 50) {
+//      console.log("BABER AZAM") 
+//     }
+
+//Task 3
+// var zipCode = prompt("Enter Your City ZipCode", 10010);
+// if (zipCode !== 10010){
+//     var userInput = prompt("Please write your city!",  "karachi");
+//     console.log(userInput);
+//     document.write(userInput);
+// }
+
+// Task 4.1
+// var x =prompt("write any number between (1,2,3,4,5,6)"); 
+// if (x === "6"){
+//    alert("congratulations") 
+// }
+// if (x !== "6"){
+//    alert("try again") 
+// }
+//Task 4.2
+// var zipCode = prompt("Enter Your City ZipCode", "10010");
+// if (zipCode === "10010"){
+//         alert("Karachi");
+//     }
+// if (zipCode !== "10010"){
+//     var userInput = prompt("Please write your city!");
+//     console.log(userInput)
+// }
+
+
+// Task 5
+// var firstName = prompt("Enter your first name");
+// if (firstName !== "abeer" ){
+//    alert("No match") 
+// }
+
+// ====== Chap 12 =======//
+
+//Task 1
+// var avgRuns = prompt("Enter a average of a palyer in a international cricket (should be under 50)",50)  
+// if (avgRuns >= 50) {
+//      alert("BABER AZAM") 
+//     }
+// if (avgRuns != 50){
+//             alert("Keep Hardwork!");
+// }
+
+// Task 2 (MarkSheet)
+// var totalMarks = 700;
+// console.log(totalMarks)
+
+// var science = +prompt("Enter your marks obtained in Science");
+// var math = +prompt("Enter your marks obtained in Math");
+// var sst = +prompt("Enter your marks obtained in Social Studies");
+// var computer = +prompt("Enter your marks obtained in Computer");
+// var physics = +prompt("Enter your marks obtained in Physics");
+// var urdu = +prompt("Enter your marks obtained in Urdu");
+// var english = +prompt("Enter your marks obtained in English");
+// console.log("Science:",science)
+// console.log("Math:",Math)
+// console.log("SST:",sst)
+// console.log("Computer:", computer)
+// console.log("Physics:", physics)
+// console.log("Urdu:",urdu)
+// console.log("English:",english)
+// var totalMarksObtain = science + math + sst + computer + physics + urdu + english;
+// console.log(totalMarksObtain)
+// var percentage = (totalMarksObtain / totalMarks) * 100;
+// console.log(percentage)
+// alert("Total Marks Obtained: " + totalMarksObtain);
+// alert("Percentage: " + percentage + "%");
+
+// if (percentage >=70 ) {
+//     console.log("A+")
+// alert("Mubarrak ho! "  + percentage + " % bani hai, chalo ab mithai khilao!");
+// } else if (percentage >= 40) {
+//     console.log("Just Pass")
+// alert("Shukar hai, pass hogaya!")
+// }
+// else 
+// {
+//         console.log("fail")
+//     alert("Buhat mehnat ki zaroorat hai. Try harder next time.");
+// }
+
+//Task 3
+// var a = prompt("Enter the value of a",10) 
+// //first correct this statement if value of true is a number write == otherwie javascript understand as string//
+// if (a == 10) { 
+//     alert("a is 10"); 
+// }else {
+//     alert("The correct value of a is ____ ")
+// } 
+
+// Task 4
+// var city = prompt("Enter Your City");
+
+// if (city === "Karachi"){
+//         alert("Acknowledging it is Karachi");
+//     }
+// else if (city === "Lahore"){
+//     alert("Acknowledging it is Lahore");
+// }
+// else{
+//     alert("please write your correct city you entered city is not match");
+// }
+
+// ====== Chap 13 =======//
+
+//Task 1
+//Selection criteria //
+// var avgRuns = prompt("Enter your average",40)
+// var strikeRate = prompt("Enter your strike rate",150)
+// if (avgRuns >= 40 && strikeRate >= 150) {
+//     alert("you're selected for national team") 
+//     }
+// else {
+//     alert("Keep Hardwork!");
+// }
+
+// var avgRuns = prompt("Enter your average",40)
+// var strikeRate = prompt("Enter your strike rate",150)
+// if (avgRuns == 40 && strikeRate == 150) {
+//     alert("Congratulation! You're Selected For National Team") 
+//     }
+// else {
+//     alert("Keep Hardwork!! Better Luck Next Time");
+// }
+
+// Task 2 
+// var totalMarks = 200;
+// var html = +prompt("Enter your marks obtained in html");
+// var css = +prompt("Enter your marks obtained in css");
+// var totalMarksObtain = html + css ;
+// var percentage = (totalMarksObtain / totalMarks) * 100;
+
+// if (percentage == 70 && percentage !== 70 || percentage >= 70 ) {
+//     alert(`Mubarrak ho! ${percentage} % bani hai,ab aap javascript ki class me bhetskte han`);
+// } 
+// else 
+// {
+//     alert("Not allow! Buhat mehnat ki zaroorat hai. Try harder next time.");
+// }
+
+//Task 3
+// var fullName = prompt("Enter your name");
+// var age = prompt("Enter your age");
+// if (fullName === "Hamza" && "Arsalan" || age < 60 ) {
+//     console.log(fullName,age);
+// } 
+
+
+// Task 4
+var higestRuns = prompt("Enter your average",100)
+var wickets = prompt("Enter your strike rate",100)
+if (higestRuns < wickets && wickets > higestRuns) {
+    alert("Congratulation! You're Bowling All rounder") 
+    }
+else {
+    alert("Need To Be Focus ON Batting");
+}
+
