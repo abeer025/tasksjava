9cf618abba460b199cda7f3d0c7cb55b50fb4e80
abeer025 <abeeriqbalahmed@gmail.com>
2024-03-24 03:28:22 +0500

    complete chap 20

--- app.js
+++ app.js
@@ -15,7 +15,8 @@
 // alert("your'e learning Javascript!");
 
 // task 3
-// alert("Welcome to Javascript");
+// Swal.fire("Welcome to Javascript")
+//  Using sweet alert refer by https://sweetalert2.github.io/ //
 
 // ====== Chap 2 =======//
 // Task 1
@@ -86,7 +87,7 @@
 // fullname = firstName + " " + lastName;
 // console.log(fullname);
 
-//Task 5
+// Task 5
 // *RULES for variable names*//
 // var asd = '' //legal
 // var 123 = '' //illegal
@@ -98,30 +99,30 @@
 
 // ====== Chap 5 =======//
 
-//Task 1
+// Task 1
 //the name of the remainder is ;
 //modulus operator '%' //
 
-//Task 2
+// Task 2
 // var num = 20 % 6; 
 // //the answer is "2" because is not divide evenly.//
 // console.log(num)
 
-//Task 3
+// Task 3
 // var largeNum = 1000 * 2000;
 // console.log(largeNum)
 
-//Task 4
+// Task 4
 // var num = 10;
 // console.log(num)
 // var substract = num - 5;
 // console.log(substract)
 
-//Task 5
+// Task 5
 // var evenlyDivide = 10 % 10
 // console.log(evenlyDivide)
 
-//Task 6
+// Task 6
 // alert("2*2 =" + 2*2)
 
 // ====== Chap 6 =======//
@@ -135,26 +136,26 @@
 // z = x++; //post-increment
 // console.log(z);
 
-//Task 2
+// Task 2
 // var x = 100;
 // y = --x; //pre-decrement is fastest way to reduce it.//
 // console.log(y);
 
-//Task 3
+// Task 3
 // var x = 50;
 // var y = x++; // the answer is same value you assign before because its post-increment//
 // console.log(y)
 
-//Task 4
+// Task 4
 // var y = 50;
 // var z = --y; // the answer 49 because its pre-decrement//
 // console.log(z)
 
-//Task 5
+// Task 5
 // var num = 50; newNum = num--; 
 // console.log(newNum);
 
-//Task 6 
+// Task 6 
 // var num = 99; newNum = ++num;
 // console.log(newNum);
 
@@ -165,15 +166,15 @@
 
 // ====== Chap 7 =======//
 
-//Task 1
+// Task 1
 // var calculatedNum = 2 + (2 * 6); // the answer is 2 * 6 = 12 + 2 = 14  //
 // console.log(calculatedNum)
 
-//Task 2
+// Task 2
 // var calculatedNum = (2 + 2) * 6; // the answer is 2 + 2 = 4 * 6 = 24  //
 // console.log(calculatedNum)
 
-//Task 3
+// Task 3
 // var calculatedNum = (2 + 2) * (4 + 2); // the answer is 2 + 2 = 4 = 4 + 2 = 6 = 4 * 6 = 24  //
 // console.log(calculatedNum)
 
@@ -181,25 +182,25 @@
 // var calculatedNum = ((2 + 2) * 4) + 2; // the answer is 2 + 2 = 4 * 4 = 18  //
 // console.log(calculatedNum)
 
-//Task 5
+// Task 5
 // var calculatedNum = (2 + 2) * (4 + 10); // the answer is 2 + 2 = 4 = 4 + 10 = 14 * 4 = 56  //
 // console.log(calculatedNum)
 
-//Task 6
+// Task 6
 // var calculatedNum = (2 + (2 * 4) + 10); // the answer is 2 * 4 = 8 + 2 + 10 + 10 = 20  //
 // console.log(calculatedNum)
 
-//Task 7
+// Task 7
 // var calculatedNum = 4 / 2 * 4; // the answer is 4 / 2 = 2 * 4 = 8   //
 // console.log(calculatedNum)
 
-//Task 7
+// Task 7
 // var calculatedNum = 4 / (2 * 4); // the answer is 4 / 2 = 2 * 4 = 8   //
 // console.log(calculatedNum)
 
 // ====== Chap 8 =======//
 
-//Task 1
+// Task 1
 // var num = "2" + "2"; // the value of num is 22 because its a string, + two string concatenating in javascript  //
 // console.log(num)
 
@@ -209,17 +210,17 @@
 // alert(message)
 // alert(`the value of message is: " ${message} " because its a string, + two string concatenating in javascript`)
 
-//Task 3
+// Task 3
 // alert("33" + 3); // the message is "33" + 3 = 333 because its a string, + two string concatenating in javascript  //
 
 // Task 4
 // alert("Pakistan " + "Zindabad"); // the message is "Pakistan " + "Zindabad" because its a string, + two string concatenating in javascript  //
 
-//Task 5
+// Task 5
 // var numWord = "K" + "2"; // the value of numWord is "K" + "2" = K2 because its a string, + two string concatenating in javascript  //
 // console.log(numWord)
 
-//Task 6
+// Task 6
 // var compact =  'A'; 
 // var size =  '4';
 // var compactSize =  compact + size ; // the answer is "A" + "4" = A4 because its a string, + two string concatenating in javascript  //
@@ -227,7 +228,7 @@
 
 // ====== Chap 9 =======//
 
-//Task 1
+// Task 1
 // var firstName = prompt("Enter your first name");
 // document.write(`Your first name is: ${firstName}`)
 
@@ -235,7 +236,7 @@
 // var country = prompt("Country?", "China");
 // document.write(`Your country is: ${country}`)
 
-//Task 3
+// Task 3
 // var yourName = prompt("Enter Your Name"); // "" inverted coma is necessary when you write a message in prompt//
 // document.write(`Your full name is: ${yourName}`)
 
@@ -243,17 +244,17 @@
 // var favoriteSports = prompt("Which sports do you like?", "Cricket");
 // document.write(`My favorite sports is: ${favoriteSports}`)
 
-//Task 5
+// Task 5
 // var hobbyQs = "What's your hobby?";
 // var hobby = "play cricket";
-//Task 6
+// Task 6
 // var userInput = prompt(hobbyQs, hobby);
 // document.write(`<p>${hobbyQs}</p>`);
 // document.write(`<p>My hobby is: ${hobby}</p>`)
 
 // ====== Chap 10 =======//
 
-//Task 1
+// Task 1
 // var city = "Karachi" 
 // if (city = "Karachi") {
     //  console.log("The City OF Lights") }
@@ -275,7 +276,7 @@
 // document.write(`The Value of z is : ${userInput} `)
 // }
 
-//Task 3
+// Task 3
 // var zipCode = prompt("Enter Your City ZipCode", 10010);
 // if (zipCode == 10010){
 //     alert("Karachi");
@@ -299,7 +300,7 @@
 
 // ====== Chap 11 =======//
 
-//Task 1
+// Task 1
 // var city = Prompt( "Enter your city:" ,"Karachi" )
 // //!== means city not equal to Karachi //
 // if (city !== "Karachi") {
@@ -313,7 +314,7 @@
 //      console.log("BABER AZAM") 
 //     }
 
-//Task 3
+// Task 3
 // var zipCode = prompt("Enter Your City ZipCode", 10010);
 // if (zipCode !== 10010){
 //     var userInput = prompt("Please write your city!",  "karachi");
@@ -329,7 +330,7 @@
 // if (x !== "6"){
 //    alert("try again") 
 // }
-//Task 4.2
+// Task 4.2
 // var zipCode = prompt("Enter Your City ZipCode", "10010");
 // if (zipCode === "10010"){
 //         alert("Karachi");
@@ -348,7 +349,7 @@
 
 // ====== Chap 12 =======//
 
-//Task 1
+// Task 1
 // var avgRuns = prompt("Enter a average of a palyer in a international cricket (should be under 50)",50)  
 // if (avgRuns >= 50) {
 //      alert("BABER AZAM") 
@@ -395,7 +396,7 @@
 //     alert("Buhat mehnat ki zaroorat hai. Try harder next time.");
 // }
 
-//Task 3
+// Task 3
 // var a = prompt("Enter the value of a",10) 
 // //first correct this statement if value of true is a number write == otherwie javascript understand as string//
 // if (a == 10) { 
@@ -419,12 +420,12 @@
 
 // ====== Chap 13 =======//
 
-//Task 1
+// Task 1
 //Selection criteria //
 // var avgRuns = prompt("Enter your average",40)
 // var strikeRate = prompt("Enter your strike rate",150)
 // if (avgRuns >= 40 && strikeRate >= 150) {
-//     alert("you're selected for national team") 
+//     Swal.fire("you're selected for national team") 
 //     }
 // else {
 //     alert("Keep Hardwork!");
@@ -433,7 +434,7 @@
 // var avgRuns = prompt("Enter your average",40)
 // var strikeRate = prompt("Enter your strike rate",150)
 // if (avgRuns == 40 && strikeRate == 150) {
-//     alert("Congratulation! You're Selected For National Team") 
+//     Swal.fire("Congratulation! You're Selected For National Team") 
 //     }
 // else {
 //     alert("Keep Hardwork!! Better Luck Next Time");
@@ -447,14 +448,14 @@
 // var percentage = (totalMarksObtain / totalMarks) * 100;
 
 // if (percentage == 70 && percentage !== 70 || percentage >= 70 ) {
-//     alert(`Mubarrak ho! ${percentage} % bani hai,ab aap javascript ki class me bhetskte han`);
+//     Swal.fire(`Mubarrak ho! ${percentage} % bani hai,ab aap javascript ki class me bhetskte han`);
 // } 
 // else 
 // {
 //     alert("Not allow! Buhat mehnat ki zaroorat hai. Try harder next time.");
 // }
 
-//Task 3
+// Task 3
 // var fullName = prompt("Enter your name");
 // var age = prompt("Enter your age");
 // if (fullName === "Hamza" && "Arsalan" || age < 60 ) {
@@ -463,11 +464,288 @@
 
 
 // Task 4
-var highestRuns = prompt("What's your Highest Score",100)
-var wickets = prompt("How many Wickets you take ",50)
-if (highestRuns < wickets && wickets > highestRuns) {
-    alert("Congratulation! You're Selected As A All rounder") 
-    }
-else {
-    alert("You're A Bowlling Allrounder & Need To Be Focus ON Batting Bettert Time");
-}
+// var highestRuns = prompt("What's your Highest Score",100)
+// var wickets = prompt("How many Wickets you take ",50)
+// if (highestRuns < wickets && wickets > highestRuns) {
+//     Swal.fire("Congratulation! You're Selected As A All rounder") 
+//     }
+// else {
+//     alert("You're A Bowlling Allrounder & Need To Be Focus ON Batting Better Luck Next Time");
+// }
+
+// ====== Chap 14 =======//
+
+// Task 1
+// var password = prompt("Enter your password");
+// if (password !== "") {
+//     if(password.length <= 5) {
+//     alert("Password must be greater than 5")
+//     }else{
+//         alert("OK")
+//     }
+// }
+
+// Task 2 
+// var a = 1;
+// c = "Max";
+// if (a === 1) {// in this condition a is not defined//
+//     if (c === "Max") {
+//     Swal.fire("OK"); 
+// } 
+//     } 
+
+
+// Task 3
+// var a = 1;
+// c = "Max";
+// if (a === 1 && c === "Max") {// in this condition a & c is not defined//
+//     Swal.fire("OK") 
+//     }
+
+// Task 4
+// var caughtAndBall = prompt("your soft decison out or not out", "out");
+// if (caughtAndBall === "out") {
+//     Swal.fire("Out!");
+//     var chancesOfOut = prompt("How much chanes of Out %", 50)
+//     if(chancesOfOut <= 50) {
+//     Swal.fire("Umpires Called")
+//     }else{
+//         Swal.fire("Not Out")
+//     }
+// }
+
+// ====== Chap 15 [I] =======//
+
+// Task 1
+// var fruits = []; //empty array//
+// console.log(fruits); 
+
+// Task 2
+// var fruits = ["Apple"];
+// console.log(fruits); // ()index 0, length 1
+
+// Task 3
+// var alphabet = ["h","i","j","k"];
+// console.log(alphabet[2]) // ()index 3, length 4
+// Swal.fire(alphabet[2]);
+
+// Task 4
+// var alphabet=["h","i","j","k","l","m","n","o"];
+// Swal.fire("The total length of a array is " + alphabet.length);
+
+// Task 5
+// var animalWithRide = ["horse"];
+// animalWithRide[1] ="Camel";
+// console.log(animalWithRide);
+// Swal.fire("New element "  +  animalWithRide[1] + " added");
+
+// ====== Chap 16 [II] =======//
+
+// Task 1
+// var animalWithRide = ["horse"];
+// console.log(animalWithRide);// before push
+// animalWithRide.push ="Camel";
+// alert("New element "  +  animalWithRide.push + " added in last");
+// console.log(animalWithRide);// after push
+
+// Task 2
+// var alphabet=["h","i","j","k"];
+// console.log(alphabet)//before pop
+// alert("Remove the last element: " + alphabet.pop() + " from the array Alphabet"  );
+// console.log(alphabet)//after pop
+
+// Task 3
+// var alphabet=["h","i","j","k"];
+// console.log(alphabet)//before push
+// alphabet.push[4] = 40;
+// alert(`Add new element " ${alphabet.push[4]} " in the last of array Alphabet`  );
+// console.log(alphabet)//after push
+
+// ====== Chap 16 [III] =======//
+
+// Task 1
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.shift(0)
+// console.log(sizes)
+
+// Task 2
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.unshift(10,20,30);
+// console.log(sizes) 
+
+// Task 3
+// var fruits = ["Mango"];
+// console.log(fruits);
+// fruits.unshift("Apricot")
+// alert(`Add new element " ${fruits[0]
+// } " in the begning of array fruits`);
+// console.log(fruits)
+
+// Task 4
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.splice(2,0,"L"); //2 is index no and 0 delete count then add new element//
+// console.log(sizes)
+
+// Task 5
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// console.log(sizes)
+// var regSizes = sizes.splice(0,3)
+// console.log(regSizes)
+
+// Task 6
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// pets.splice(1,3,"crocodile","monkey");//1 is index no and 3 delete count then add 2 new element
+// console.log(pets);
+
+// Task 7
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// console.log(pets);
+// var petsUpdate = pets.splice(1,2);//1 is index no and 3 delete count then add 2 new element
+// console.log(petsUpdate);// using splice method
+// console.log(pets);
+
+// Task 8
+// var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+// console.log(pets);
+// petsReduce = pets.slice(3,5);//range of index no is 0 to 3 means index no 0,1 and 2 delete
+// console.log(petsReduce);
+
+// ====== Chap 17-20  =======//
+
+// Task 1
+// for(var i = 0; i < 10; i++){
+//     console.log(`loop ${i}`) 
+// } // when you say loop run 10 times its limition run 9 time because its never run complete limitation//
+
+// Task 2
+// usual counter name (i), usual starting value (0), and usual increment (1)
+// for(var i = 0; i <= 12; i++){
+//     console.log(`usualCounter(i) ${i}`)
+// }
+
+// Task 3
+// What are the 5 characters missing from this code, excluding any spaces that are missing? 
+//Type them in order, with no spaces or commas between them. 
+//for var i = 0 i <= 4 i++ 
+//Note: Complete this statement by yourself:
+
+// for (var i = 0; i <= 4; i++) {
+// }
+// //character no 1 ()
+// //character no 2 =
+// //character no 3 ; 
+// //character no 4 +
+// //character no 5 {}
+
+// Task 4
+// for(var runs = 0; runs <100; runs++){
+//     console.log(`Runs ${runs}`)
+// }
+
+// Task 5
+// for (var i = 3; i > 0; i--) {
+// console.log(i)} // in this case usual starting with 3 and reverse withe dicrement
+
+// Task 6
+// for (var i = 1; i <10; i++) {
+// console.log(`Index: ${i}`)} // The statement assigns the number of elements in the array to the variable.
+// //its mean how many times you run loop 
+
+// Task 7
+// var flag = true;
+
+// Task 8
+// var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+// console.log(pets);
+// for (var i = 0; i < pets.length; i++) {
+// console.log(pets[i])
+// }
+
+// Task 9
+// In this case not break counter runing till counter length not end
+// for (var i = 0; i < 10; i++) {
+    // console.log(i)
+    // alert(i)
+    // }
+    
+// In this case break counter  when its reached i = 1
+// for (var i = 0; i < 10; i++) {
+// console.log(i)
+// if (i = 1){
+// alert(i)
+// break
+// }
+// }
+
+// Task 10
+// var userNames = [
+//     "Abeer",
+//     "Bilal",
+//     "Ali",
+//     "Basit",
+//     "Arshad",
+//     "Umair",
+//     "Mohsin",
+//     "Abduallah",
+//     "Siddique",
+// ];
+
+// var firstName = prompt("Enter your first name");
+// var flag = false; 
+
+// for (var i = 0; i < userNames.length; i++) {
+//     if (firstName === userNames[i]) {
+//         console.log(firstName);
+//         Swal.fire("Enter");
+//         flag = true;
+//         break; 
+//     }
+// }
+// if (flag === false) {
+//     console.log(firstName);
+//     alert("Please write correct user name");
+// }
+
+// Task 11
+//first declare a variable for list//
+// var list = ["5", "7", "9", "3", "0",]
+// //second declare a variable for prompt t take user input//
+// var userInput = prompt("enter a random number between 0 to 9")
+// var matchFound = false;
+// for (var i = 0; i < list.length; i++) {
+//     if (userInput === list[i]) {
+//         alert("Match found");
+//         matchFound = true;
+//         break;
+//     }
+// };
+// //need to compelete this code for given if not match using flag bolean
+// if (matchFound === false) {
+//     console.log(userInput);
+//     Swal.fire("try again");
+// } 
+
+// Task 12
+// #1
+// var firstArr = ["a","b","c","d","e","f"]
+// var secondArr = [1,2,3,4,5,6]
+
+// for (var i=0; i< firstArr.length; i++){
+//     for (var j = 0; j < secondArr.length; j++){
+//     var mixArr =(firstArr[i]+secondArr[j])
+//     console.log(mixArr)
+
+// }
+// }
+
+// #2
+// var firstArr = ["a","b","c","d","e","f"]
+// var secondArr = [1,2,3,4,5,6]
+// var mixArr = []
+
+// for (var i=0; i< firstArr.length; i++){
+//     for (var j = 0; j < secondArr.length; j++){
+//     console.log(firstArr[i], secondArr[j])
+//     mixArr.push (firstArr[i],secondArr[j])
+// }
+// }
